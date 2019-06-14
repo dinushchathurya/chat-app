9ce17ceb5475161d10941e0e39b3bf85066bb8f6
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-14 22:05:31 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <title>Login to your Account</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatiable" content="IE-edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Courgette|Pacifico:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootsrapcdn.com/bootsrap/3.3.7/css/bootsrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootsrapcdn.com/bootsrap/3.3.7/js/bootsrap.min.js"></script>
 </head>
 <body>
-
+    <div class="signin-form">
+    	<form action="" method="post">
+    		<div class="form-header">
+    			<h2>Sign In</h2>
+    			<p>Login in to MyChat</p>
+    		</div>
+    		<div class="form-group">
+    			<label>Email</label>
+    			<input type="email" name="email"class="form-control" placeholder="example@gmail.com" autocomplete="off" required>
+    		</div>
+    		<div class="form-group">
+    			<label>Email</label>
+    			<input type="password" name="pass"class="form-control" placeholder="Enter your password here" autocomplete="off" required>
+    		</div>
+    		<div class="small">Forgot password?<a href="forgot_pass.php">Click Here</a>
+    		</div><br>
+    	</form>
+    </div>
 </body>
 </html>
