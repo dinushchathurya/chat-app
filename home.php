99ea9e98fe2a75880814f368fa0c424f7c5bfbcb
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-16 22:01:43 +0530

    home.php

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
 <body>
      <div class="container main-section">
@@ -56,13 +57,83 @@
      					<div class="right-header_detail">
      						<form method="post">
      							<p><?php echo "$user_name";?></p>
-     							<span></span>
+     							<span><?php echo $total; ?> messages</span>&nbsp &nbsp
+     							<button name="logout" class="btn btn-danger">Logout</button>
      						</form>
+     						<?php 
+     						  if(isset($_POST['logout'])){
+     						  	$update_msg=mysqli_query($con,"UPDATE users SET log_in='Offline' WHERE user_name='$user_name'");
+     						  	header("Location:logout.php");
+     						  	exit();
+     						  }
+     						?>
      					</div>
+     				</div>
+     			</div>
+     			<div class="row">
+     				<div id="scrolling_to_bottom" class="col-md-12 right-header-contentChat">
+     					<?php $update_msg = mysqli_query($con, "UPDATE users_chats SET msg_status='read' WHERE sender_username=$'username' AND receiver_username='$username'");
+     					$sel_msg = "select * from user_chats where (sender_username='$username' AND receiver_username='$username') OR (receiver_username='$username' AND sender_username='$username') ORDER BY 1 ASC";
+     					$run_msg =mysqli_query($con, $sel_msg);
+     					while ($row = mysqli_fetch_array($run_msg)){
+     						$sender_username=$row['sender_username'];
+     						$receiver_username=['receiver_username'];
+     						$msg_content= $row['msg_content'];
+     						$msg_date =$row['msg_date'];
+     				
+     					 ?>
+     				    <ul>
+     				    	<?php 
+     				    	if($user_name==$receiver_username AND $user_name == $sender_username){
+     				    		echo" <li>
+     				    				<div class='rightside-chat'>
+     				    					<span>$username<small>$msg_date</small></span>
+     				    					<p>$msg_content</p>
+     				    				</div>
+     				    		      </li> ";
+     				    	}
+     				    	else if($user_name==$sender_username AND $user_name == $receiver_username){
+     				    		echo" <li>
+     				    				<div class='rightside-chat'>
+     				    					<span>$username<small>$msg_date</small></span>
+     				    					<p>$msg_content</p>
+     				    				</div>
+     				    		      </li> ";
+     				    	}  
+     				    	?>
+     				    	<?php 
+     				        }
+     				    	?>
+     				    </ul>
+     				</div>
+     			</div>
+     			<div class="row">
+     				<div class="col-md-12 right-chat-textbox">
+     					<form method="post">
+     						<input autocomplete="off" type="text" name="msg_content" placeholder="Write your message....">
+     						<button class="btn" name="submit"><i class="fa fa-telegram" aria-hidden="true"></i></button>
+     					</form>
      				</div>
      			</div>
      		</div>
      	</div>
      </div>
+     <?php 
+       if(isset($_POST['submit'])){
+       	 $msg = htmlentities($_POST['msg_content']);
+       	 if($msg == ""){
+       	 	echo" <div class='alert alert-danger'>
+       	 	<strong><center>Message was unable to send.</center></strong></div>";
+       	 }
+       	 else if (strlen($msg)>100){
+       	 	echo" <div class='alert alert-danger'>
+       	 	<strong><center>Message is too long.Use only 100characters.</center></strong></div>";
+       	 }
+       	 else{
+       	 	$insert = "insert into users_chats (sender_username,receiver_username,msg_content,msg_status,msg_date) values ('$user_name','$username','$msg','unread', NOW ())";
+       	 	$run_insert =mysqli_query($con,$insert);
+       	 }
+       }
+       ?>
 </body>
 </html>
